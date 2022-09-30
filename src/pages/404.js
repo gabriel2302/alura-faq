8e2d4d0c2909f041d5eb9357a0ef91a2a08b558c
glacerda <glacerda@cidadeimperial.com.br>
2022-09-30 13:39:13 -0300

    first commit

---
+++ src/pages/404.js
@@ -0,0 +1,12 @@
+import Link from "../components/Link";
+
+export default function FAQPage() {
+  return (
+    <div>
+      <h1>Você se perdeu e caiu na página 404 :O</h1>
+      <Link href="/">
+        Ir para home
+      </Link>
+    </div>
+  )
+}
---
+++ src/pages/index.js
@@ -0,0 +1,35 @@
+// import Link from "../components/Link";
+// import Footer from "../components/Footer";
+
+import HomeScreen from "../screens/HomeScreen";
+
+export default HomeScreen;
+// function Title({children, as}) {
+//   const Tag = as;
+//   return (
+//     <>
+//       <Tag>{children}</Tag>
+//       <style jsx>
+//         {
+//           `
+//             ${Tag} {
+//               color: red;
+//             }
+//           `
+//         }
+//       </style>
+//     </>
+//   )
+// }
+
+// export default function HomePage() {
+//   return (
+//     <div>
+//         <Title as="h1">Alura cases - Home page</Title>
+//         <Link href="/faq">
+//           Ir para FAQ
+//         </Link>
+//         <Footer />
+//     </div>
+//   )
+// }
